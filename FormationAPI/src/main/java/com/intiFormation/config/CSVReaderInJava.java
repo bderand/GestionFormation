515ba4b1b36d99adc6ddd5652e61d4328b19af9e
Thiou <mathieu.carillat@gmail.com>
2022-12-20 11:29:14 +0100

    CSV Personne

---
+++ FormationAPI/src/main/java/com/intiFormation/config/CSVReaderInJava.java
@@ -0,0 +1,48 @@
+package com.intiFormation.config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.intiFormation.model.Personne;
+import com.intiFormation.service.IPersonneService;
+import com.intiFormation.service.PersonneService;
+
+public class CSVReaderInJava {
+
+	private static IPersonneService pservice = new PersonneService();
+	
+    public static void main(String... args) {
+        readPersonnesFromCSV("src/main/java/com/intiFormation/config/personnes.txt");
+    }
+
+    private static void readPersonnesFromCSV(String fileName) {
+        Path pathToFile = Paths.get(fileName);
+
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] attributes = line.split(",");
+                Personne p = createPersonne(attributes);
+                pservice.ajout(p);
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private static Personne createPersonne(String[] metadata) {
+    	int id = Integer.parseInt(metadata[0]);
+    	String prenom = metadata[1];
+    	String nom = metadata[2];
+        int age = Integer.parseInt(metadata[3]);
+        String email = metadata[4];
+        String tel = metadata[5];
+        return new Personne(id, prenom, nom, age, email, tel);
+    }
+
+}
